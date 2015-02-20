bd46df02f46ed2b8896bc9c657a42836df28a0a2
ed wassermann <vis0vis@gmail.com>
2015-02-20 16:15:12 +0100

    Adds master detail for profile/repos

--- js/app.js
+++ js/app.js
@@ -1,30 +1,45 @@
 var devs = [
-  { login: 'wycats', name: 'Yehuda Katz' },
-  { login: 'tomdale', name: 'Tom Dale' },
-  { login: 'vis-kid', name: 'H. Bogart' }
+{ login: 'wycats', name: 'Yehuda Katz' },
+{ login: 'tomdale', name: 'Tom Dale' },
+{ login: 'vis-kid', name: 'H. Bogart' }
 ]
-  
+
 
 Github = Ember.Application.create();
 
 Github.Router.map(function() {
-	this.resource("user", {path: "/users/:login"});
+	this.resource("user", {path: "/users/:login"}, function() {
+    this.resource('repositories');
+	});
 });
 
 Github.IndexRoute = Ember.Route.extend({
-  model: function() { return devs }
+	model: function() { return devs }
 });
 
 Github.IndexController = Ember.ArrayController.extend({
 	actions: {
-  	clickedSearchButton: function(){
-      alert("I've been clicked");
-    }
-	}
+						 clickedSearchButton: function(){
+																		alert("I've been clicked");
+																	}
+					 }
 });
 
 Github.UserRoute = Ember.Route.extend({
-  model: function(params) {
-		return Ember.$.getJSON("https://api.github.com/users/" + params.login);
+	model: function(params) {
+					 return Ember.$.getJSON("https://api.github.com/users/" + params.login);
+				 }
+});
+
+Github.UserIndexRoute = Ember.Route.extend({
+	model: function(){
+					 return this.modelFor('user');
+				 }
+});
+
+Github.RepositoriesRoute = Ember.Route.extend({
+  model: function() {
+    var user = this.modelFor('user');
+		return Ember.$.getJSON(user.repos_url);
 	}
 });
