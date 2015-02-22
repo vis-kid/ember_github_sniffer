d2fa65ef9024e5e395cd2ae2334730445b3bcb6f
ed wassermann <vis0vis@gmail.com>
2015-02-22 20:56:26 +0100

    Some cleanup

--- js/app.js
+++ js/app.js
@@ -1,65 +1,64 @@
 var devs = [
-{ login: 'wycats', name: 'Yehuda Katz' },
-{ login: 'tomdale', name: 'Tom Dale' },
-{ login: 'vis-kid', name: 'H. Bogart' }
+  { login: 'wycats', name: 'Yehuda Katz' },
+  { login: 'tomdale', name: 'Tom Dale' },
+  { login: 'vis-kid', name: 'H. Bogart' }
 ]
-
 
 Github = Ember.Application.create();
 
 Github.Router.map(function() {
-	this.resource("user", {path: "/users/:login"}, function() {
+  this.resource("user", {path: "/users/:login"}, function() {
     this.resource('repositories');
-		this.resource('repository', { path: 'repositories/:reponame'});
-	});
+    this.resource('repository', { path: 'repositories/:reponame'});
+  });
 });
 
 Github.IndexRoute = Ember.Route.extend({
-	model: function() { return devs }
+  model: function() { return devs }
 });
 
 Github.IndexController = Ember.ArrayController.extend({
-	actions: {
-						 clickedSearchButton: function(){
-																		alert("I've been clicked");
-																	}
-					 }
+  actions: {
+    clickedSearchButton: function(){
+      alert("I've been clicked");
+    }
+	}
 });
 
 Github.UserRoute = Ember.Route.extend({
-	model: function(params) {
-					 return Ember.$.getJSON("https://api.github.com/users/" + params.login);
-				 }
+  model: function(params) {
+    return Ember.$.getJSON("https://api.github.com/users/" + params.login);
+  }
 });
 
 Github.UserIndexRoute = Ember.Route.extend({
-	model: function(){
-					 return this.modelFor('user');
-				 }
+  model: function(){
+    return this.modelFor('user');
+  }
 });
 
 Github.RepositoriesRoute = Ember.Route.extend({
   model: function() {
     var user = this.modelFor('user');
-		return Ember.$.getJSON(user.repos_url);
-	}
+    return Ember.$.getJSON(user.repos_url);
+  }
 });
 
 Github.RepositoriesController = Ember.ArrayController.extend({
   needs: ['user'],
-	user: Ember.computed.alias('controllers.user')
+  user: Ember.computed.alias('controllers.user')
 });
 
 Github.RepositoryRoute = Ember.Route.extend({
   model: function(){
     var user = this.modelFor('user');
-		var url = 'https://api.github.com/repos' + user.login + '/' + params.reponame;
-		return Ember.$.getJSON(url);
-	}
+    var url = 'https://api.github.com/repos' + user.login + '/' + params.reponame;
+    return Ember.$.getJSON(url);
+  }
 });
 
 Ember.Handlebars.registerBoundHelper('fromDate', function(theDate) {
   var today = moment();
-	var target = moment(theDate);
-	return target.from(today);
+  var target = moment(theDate);
+  return target.from(today);
 });
